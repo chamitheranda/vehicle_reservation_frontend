39be2d2ad6340b4494f6726ec198277f68904e5b
chamith <erandachamith322@gmail.com>
2023-10-03 00:54:35 +0530

    create html structure

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './nav.css'
+import React from 'react';
+import './nav.css';
+
 
 const Nav = () => {
-  return (
-    <div>
-      Nav
+  return ( 
+    <div className='nav_bar'>
+      <p className='welcome_msg'>Welcome To Vehiclo!!</p>
+
+      <div className='buttons'>
+        <div>
+          <button onclick="#" className='login_button'>Sign in</button>
+        </div>
+
+        <div> 
+          <button onclick="#" className='signup_button'>Sign UP</button>
+        </div>
+      </div>
     </div>
   )
 }
