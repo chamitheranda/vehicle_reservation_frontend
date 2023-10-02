a9004c9e8baf4653eb38139496d169d94fd5f289
chamith <erandachamith322@gmail.com>
2023-10-03 02:08:25 +0530

    create event listner

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import './nav.css';
 
-
 const Nav = () => {
-  return ( 
+  const handleSignInClick = () => {
+    window.location.href = 'http://localhost:8080/swagger-ui.html';
+  };
+
+  return (
     <div className='nav_bar'>
       <p className='welcome_msg'>Welcome To Vehiclo!!</p>
 
       <div className='buttons'>
         <div>
-          <button onclick="#" className='login_button'>Sign in</button>
+          <button onClick={handleSignInClick} className='login_button'>Sign in</button>
         </div>
 
-        <div> 
-          <button onclick="#" className='signup_button'>Sign UP</button>
+        <div>
+          <button onClick={() => { /* Handle sign-up logic here */ }} className='signup_button'>Sign UP</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
