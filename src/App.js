b7153116c29002cf7c9f3aa1967afab045356244
chamith <erandachamith322@gmail.com>
2023-10-03 22:43:18 +0530

    modify the code for store jwt

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React from 'react'
-import { Form , Nav } from './components'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { Form, Nav } from './components';
+import './App.css';
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
   return (
-    <div className='App'>
+    <Auth0Provider
+      domain="dev-m8vl5qthokuutecv.us.auth0.com"
+      clientId="Vh7JFCAyC1Omz6oDdyrpmcItVen9F8g9"
+      redirectUri={window.location.origin}
+    >
+      <div className='App'>
         <div>
-          <Nav />
+          <Auth0AppContent />
         </div>
-        <div>
-          <Form />
-        </div>
-    </div>
-  )
+      </div>
+    </Auth0Provider>
+  );
 }
 
-export default App
+const Auth0AppContent = () => {
+  const { isAuthenticated, isLoading, getAccessTokenSilently, loginWithRedirect, logout } = useAuth0();
+  const [jwt, setJwt] = useState(null);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getAccessToken();
+    }
+  }, [isAuthenticated]);
+
+  const getAccessToken = async () => {
+    try {
+      const accessToken = await getAccessTokenSilently();
+      setJwt(accessToken); // Store the JWT token in state
+    } catch (error) {
+      if (error.error === 'login_required') {
+        // The user needs to log in again
+        loginWithRedirect();
+      } else {
+        console.error('Error obtaining access token:', error);
+      }
+    }
+  };
+
+  if (isLoading) {
+    // You can optionally show a loading indicator while Auth0 is checking authentication.
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {isAuthenticated ? <Form jwt={jwt} /> : <Nav />}
+    </div>
+  );
+}
+
+export default App;
