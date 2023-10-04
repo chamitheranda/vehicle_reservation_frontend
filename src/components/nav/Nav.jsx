dfdeb87e673f3f27731912dfbd164773406ff4dc
chamith <erandachamith322@gmail.com>
2023-10-04 18:09:21 +0530

    implement sign up functionality

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react'; // Import useAuth0 hook
+import { useAuth0 } from '@auth0/auth0-react';
 import './nav.css';
 
 const Nav = () => {
-  const { loginWithRedirect } = useAuth0(); // Get the loginWithRedirect function
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   const handleSignInClick = () => {
-    // Use loginWithRedirect to initiate the login process and redirect to Auth0 login page
     loginWithRedirect();
+  };
+
+  const handleSignUpClick = () => {
+    // Check if the user is authenticated before logging out
+    if (isAuthenticated) {
+      logout({ returnTo: window.location.origin });
+    } else {
+      // Handle sign-up logic here if the user is not authenticated
+      // For example, you can redirect the user to a sign-up page.
+    }
   };
 
   return (
     <div className='nav_bar'>
       <p className='welcome_msg'>Welcome To Vehiclo!!</p>
-
       <div className='buttons'>
+        <div>
+          <button onClick={() => { /* Handle reservation logic here */ }} className='reservation'>Make Reservation</button>
+        </div>
         <div>
           <button onClick={handleSignInClick} className='login_button'>Sign in</button>
         </div>
-
         <div>
-          <button onClick={() => { /* Handle sign-up logic here */ }} className='signup_button'>Sign UP</button>
+          <button onClick={handleSignUpClick} className='signup_button'>
+            {isAuthenticated ? 'Sign Out' : 'Sign Up'}
+          </button>
         </div>
       </div>
     </div>
