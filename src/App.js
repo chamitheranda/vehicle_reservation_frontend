0be6ba58103996a9458bb653156b379e6719fcb5
chamith <erandachamith322@gmail.com>
2023-10-09 01:36:49 +0530

    modify return section for render navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Nav } from './components'; // Import your Form and Nav components
+import { Form, Nav , NavBar } from './components'; // Import your Form and Nav components
 import './App.css';
 import { AuthProvider, useAuthContext } from '@asgardeo/auth-react'; // Import Asgardeo authentication components
 import Cookies from 'js-cookie';
 import jwt_decode from 'jwt-decode';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 
 const App = () => {
   return (
@@ -26,9 +27,20 @@ const App = () => {
       </Router>
     </AuthProvider>
   );
+  
 }
 
+
+
+
 const AsgardeoAppContent = () => {
+
+  const[showForm , setShowForm] = useState(false);
+  const toggleForm = () =>{
+    setShowForm(!showForm);
+  }
+
+
   const { state, getAccessToken, getIDToken, signOut } = useAuthContext(); 
   const [jwtToken, setJwtToken] = useState(null);
   const [idToken, setIdToken] = useState(null);
@@ -70,16 +82,24 @@ const AsgardeoAppContent = () => {
   };
 
   return (
-    <Routes>
-      <Route
-        path="/form"
-        element={state.isAuthenticated ? <Form jwtToken={jwtToken} idToken={idToken} /> : <Navigate to="/" />}
-      />
-      <Route
-        path="/"
-        element={state.isAuthenticated ? <Nav />: <Nav onLogout={handleLogout} />}
-      />
-    </Routes>
+    // <Routes>
+    //   <Route
+    //     path="/form"
+    //     element={state.isAuthenticated ? <Form jwtToken={jwtToken} idToken={idToken} /> : <Navigate to="/" />}
+    //   />
+    //   <Route
+    //     path="/"
+    //     element={state.isAuthenticated ? <Nav />: <Nav onLogout={handleLogout} />}
+    //   />
+    // </Routes>
+    <>
+    <div>
+      <NavBar />
+    </div>
+    <div>
+      {state.isAuthenticated && toggleForm ? (<Form jwtToken={jwtToken} idToken={idToken}   />) : (<Nav toggleForm={toggleForm}/>)}
+    </div>
+    </>
   );
 }
 
