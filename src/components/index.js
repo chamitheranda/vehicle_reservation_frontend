73a5723a4a24a2389c5e10045881015273cec96b
chamith <erandachamith322@gmail.com>
2023-10-09 06:05:42 +0530

    export dashborad component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Nav} from './nav/Nav'
 export {default as Form} from './form/Form'
 export {default as NavBar} from './navBar/NavBar'
+export {default as Dashboard} from './dashboard/Dashboard'
