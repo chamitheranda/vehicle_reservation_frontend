28a0568b229305aff348453b8e07571380f02c01
chamith <erandachamith322@gmail.com>
2023-10-10 02:40:55 +0530

    create footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+// Footer.jsx
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content" >
+        <div className="footer-logo">
+          <h2>Vehiclo</h2>
+          <p>Vehiclo is your trusted company for making vehicle service reservations. Our mission is to provide convenient and efficient service scheduling for your vehicles.</p>
+        </div>
+        <div className="footer-contact" >
+          <h3>Contact Us</h3>
+          <p>Email: contact@vehiclo.com</p>
+          <p>Phone: +123-456-7890</p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Vehiclo</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
