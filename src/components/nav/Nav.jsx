a12a5fd95907e270ea1c66aed6635cc18baa119e
chamith <erandachamith322@gmail.com>
2023-10-10 18:21:47 +0530

    remove previous sign in and sign out metods and get these methods from parent component and render it

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import { useAuthContext } from '@asgardeo/auth-react'; // Import Asgardeo hook
+import { useAuthContext } from '@asgardeo/auth-react'; 
 import './nav.css';
 
-
-
 const Nav = (props) => {
-  const {toggleForm} = props
-  const { signIn, signOut, state } = useAuthContext(); 
-
-  const handleSignInClick = () => {
-    if(state.isAuthenticated){
-      alert("You are already logged in !!!")
-    } else{
-      signIn();
-    }  
-  };
-
-  const handleSignUpClick = () => {
-    if (state.isAuthenticated) {
-      if (window.confirm('Are you sure you want to log out?')) {
-        document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        signOut();
-      }
-    } else {
-    }
-  };
-
+  const {handleSignOutClick , handleSignInClick} = props
+  const { state } = useAuthContext(); 
 
   return (
     <div className='nav_bar'>
@@ -36,7 +15,7 @@ const Nav = (props) => {
           <button onClick={handleSignInClick} className='login_button'>Sign in</button>
         </div>
         <div>
-          <button onClick={handleSignUpClick} className='signup_button'>
+          <button onClick={handleSignOutClick} className='signup_button'>
             {state.isAuthenticated ? 'Sign Out' : 'Sign Up'}
           </button>
         </div>
