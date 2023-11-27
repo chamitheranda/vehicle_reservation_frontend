13713b99d9fd9e2b916e74d71281912f5318235f
chamith <erandachamith322@gmail.com>
2023-11-27 14:35:07 +0530

    fetch csrf token from server and modify the useEffect hooks to manage the behaviors of the parent component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Nav , NavBar , Dashboard , ReservationList , FutureRes , Footer , Profile} from './components'; 
+import React, { useState, useEffect, useCallback } from 'react';
+import { Form, Nav, NavBar, Dashboard, ReservationList, FutureRes, Footer, Profile } from './components';
 import './App.css';
-import { AuthProvider, useAuthContext } from '@asgardeo/auth-react'; 
+import { AuthProvider, useAuthContext } from '@asgardeo/auth-react';
 import Cookies from 'js-cookie';
 import jwt_decode from 'jwt-decode';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import axios from 'axios';
-
-
 
 const App = () => {
   return (
     <AuthProvider
       config={{
-        clientID: //put your client id, 
-        baseUrl: // put your baseURL, 
+        clientID: //replace with your clientID,
+        baseUrl:  //replace with your baseURL,
         signInRedirectURL: window.location.origin,
         signOutRedirectURL: window.location.origin,
-        scope: ["openid", "profile"]
+        scope: ['openid', 'profile'],
       }}
     >
       <Router>
@@ -29,78 +27,60 @@ const App = () => {
       </Router>
     </AuthProvider>
   );
-  
-}
+};
 
 const AsgardeoAppContent = () => {
+  const [userDetails, setUserDetails] = useState(null);
+  const [csrfToken, setCsrfToken] = useState(null);
+  const { state, getAccessToken, getIDToken, signOut, signIn } = useAuthContext();
+  const [jwtToken, setJwtToken] = useState(null);
+  const [idToken, setIdToken] = useState(null);
 
-  const [userDetails , setUserDetails] = useState(null);
-
-    const fetchUserDetails = async (jwtToken) => {
-      try {
-        const response = await axios.get('https://api.asgardeo.io/t/orgueejs/oauth2/userinfo', {
-          headers: {
-            Authorization: `Bearer ${jwtToken}`,
-          },
-        });
-
-        const userDetails = response.data;
-        setUserDetails(userDetails);
-      } catch (error) {
-        console.error('Error fetching user details:', error);
-        console.log('Response Data:', error.response.data);
-      }
-    };
-
-   
-
-  const [showProfile , setShowProfile] = useState(false)
-  const togglePro = () =>{
-    if(!showProfile){
-    setShowNav(showProfile);
-    setShowRes(showProfile);
-    setShowFutureRes(showProfile);
-    setShowForm(showProfile);
-    setShowDash(showProfile);
+  const [showProfile, setShowProfile] = useState(false);
+  const togglePro = () => {
+    if (!showProfile) {
+      setShowNav(showProfile);
+      setShowRes(showProfile);
+      setShowFutureRes(showProfile);
+      setShowForm(showProfile);
+      setShowDash(showProfile);
     }
     setShowProfile(!showProfile);
-  }
+  };
 
-  const[showForm , setShowForm] = useState(false);
-  const toggleForm = () =>{
+  const [showForm, setShowForm] = useState(false);
+  const toggleForm = () => {
     setShowNav(showForm);
     setShowRes(showForm);
     setShowFutureRes(showForm);
     setShowProfile(showForm);
     setShowForm(!showForm);
     setShowDash(!showForm);
-  }
+  };
 
-
-  const [showFutureRes , setShowFutureRes] = useState(false)  
-  const toggleFutureRes = () =>{
+  const [showFutureRes, setShowFutureRes] = useState(false);
+  const toggleFutureRes = () => {
     setShowNav(showFutureRes);
     setShowForm(showFutureRes);
     setShowRes(showFutureRes);
     setShowProfile(showFutureRes);
     setShowDash(!showFutureRes);
     setShowFutureRes(!showFutureRes);
-  }
+  };
 
-  const[showRes , setShowRes] = useState(false);
-  const toggleRes = () =>{
+  const [showRes, setShowRes] = useState(false);
+  const toggleRes = () => {
     setShowNav(showRes);
     setShowFutureRes(showRes);
     setShowForm(showRes);
     setShowProfile(showRes);
     setShowRes(!showRes);
     setShowDash(!showRes);
-    
-  }
+  };
 
-  const[showNav , setShowNav] = useState(false);
-  const toggleNav = () =>{
-    if(!showNav){
+  const [showNav, setShowNav] = useState(false);
+  const toggleNav = () => {
+    if (!showNav) {
       setShowDash(showNav);
       setShowForm(showNav);
       setShowRes(showNav);
@@ -108,12 +88,11 @@ const AsgardeoAppContent = () => {
       setShowProfile(showNav);
     }
     setShowNav(!showNav);
-    
-  }
+  };
 
-  const[showDash , setShowDash] = useState(false);
-  const toggleDash = () =>{
-    if(!showDash){
+  const [showDash, setShowDash] = useState(false);
+  const toggleDash = () => {
+    if (!showDash) {
       setShowNav(showDash);
       setShowForm(showDash);
       setShowRes(showDash);
@@ -121,116 +100,211 @@ const AsgardeoAppContent = () => {
       setShowProfile(showDash);
     }
     setShowDash(!showDash);
-    
-  }
+  };
 
+  const checkTokenExpiration = useCallback(() => {
+    const jwtToken = Cookies.get('jwtToken');
+    const idToken = Cookies.get('idToken');
 
-  const { state, getAccessToken, getIDToken, signOut ,signIn} = useAuthContext(); 
-  const [jwtToken, setJwtToken] = useState(null);
-  const [idToken, setIdToken] = useState(null);
+    if (jwtToken && idToken) {
+      const decodedToken = jwt_decode(jwtToken);
+      const currentTime = Date.now() / 1000;
+
+      if (decodedToken.exp < currentTime) {
+        Cookies.remove('jwtToken');
+        Cookies.remove('idToken');
+        Cookies.remove('csrfToken')
+        setJwtToken(null);
+        setIdToken(null);
+        setCsrfToken(null);
+      }
+    }
+  }, []);
+
 
   useEffect(() => {
-    const checkTokenExpiration = () => {
-      const jwtToken = Cookies.get('jwtToken');
-      const idToken = Cookies.get('idToken');
+    console.log("mount 2 useEffect");
+    const fetchData = async () => {
+      console.log("inside fetchData()");
+      try {
+        if (csrfToken === null && state.isAuthenticated) { // This condition is commented out as it seems unnecessary
+          console.log("inside the csrf logic");
+          const response = await fetch('http://localhost:8080/api/v1/csrf/get-csrf', {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+              'Content-Type': 'application/json',
+              'XCSRFTOKEN': "CsrfToken"
+            },
+          });
+          
+          console.log("get csrf token");
 
-      if (jwtToken && idToken) {
-        fetchUserDetails(jwtToken);
-        const decodedToken = jwt_decode(jwtToken);
-        const currentTime = Date.now() / 1000;
-
-        if (decodedToken.exp < currentTime) {
-          Cookies.remove('jwtToken');
-          Cookies.remove('idToken');
-          setJwtToken(null);
-          setIdToken(null);
+          if (response.ok) {
+            const token3 = await response.json();
+            const data = token3.token;
+            setCsrfToken(data);
+            setJwtToken(jwtToken);
+            setIdToken(idToken);
+            console.log("csrf token === "+data);
+            console.log(response);
+          } else {
+            console.error('Failed to fetch CSRF token:', response.status);
+          }
+        } else {
+          console.error("CSRF token already taken");
         }
+      } catch (error) {
+        console.error('Error fetching CSRF token:', error);
       }
     };
 
+    if(csrfToken === null){
+      fetchData()
+    }
+  
+  }, [csrfToken  ]); // Include the necessary dependencies
+
+  useEffect(() => {
+    console.log("mount 1 useEffect");
+    const fetchUserDetails = async () => {
+
+      if (userDetails === null && jwtToken !== null ) {
+        console.log("inside the condition");
+        try {
+          const response = await axios.get('https://api.asgardeo.io/t/orgueejs/oauth2/userinfo', {
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          });
+  
+          const userDetails = response.data;
+          setUserDetails(userDetails);
+          console.log("get user information ");
+
+        } catch (error) {
+          console.error('Error fetching user details:', error);
+          console.log('Response Data:', error.response.data);
+        }
+      } 
+    };
   
     if (state.isAuthenticated) {
-      getAccessToken().then((accessToken) => {
+      fetchUserDetails();
+    }
+  }, [jwtToken ]);
+  
+  useEffect(() => {
+    console.log("mount 3 useEffect");
+    if (state.isAuthenticated && jwtToken === null) {
+      getAccessToken().then((jwtToken) => {
         getIDToken().then((idToken) => {
-          Cookies.set('jwtToken', accessToken, {
+          Cookies.set('jwtTokenCookie', jwtToken, {
             expires: new Date(idToken.exp * 1000),
-            httpOnly: true  
+            // httpOnly: true,
+            secure: true,
           });
-          Cookies.set('idToken', idToken, { 
-            expires: new Date(idToken.exp * 1000) , 
-            httpOnly=true 
+          Cookies.set('idTokenCookie', idToken, {
+            expires: new Date(idToken.exp * 1000),
+            // httpOnly: true,
+            secure: true,
           });
-          setJwtToken(accessToken);
+          Cookies.set('csrfTokenCookie', csrfToken, {
+            expires: new Date(idToken.exp * 1000),
+            // httpOnly: true,
+            secure: true,
+          });
+         
+
+
+
+          setJwtToken(jwtToken);
           setIdToken(idToken);
+          // setCsrfToken(data);
           checkTokenExpiration();
+          console.log("jwt token = "+ Cookies.get("jwtTokenCookie"));
+          console.log("id token ="+Cookies.get("idTokenCookie"));
+          console.log("csrf token = "+Cookies.get("csrfTokenCookie"));
+          console.log("csrf token = " + csrfToken)
         });
       });
+      // if (state.isAuthenticated && jwtToken) {
+      //     fetchUserDetails(jwtToken);
+      // }
+      // fetchData();
+      
+      
     }
-  }, [state.isAuthenticated, getAccessToken, getIDToken]);
+  }, [state.isAuthenticated, getAccessToken, csrfToken]);
 
+
+
+  
 
   const handleSignOutClick = () => {
     if (state.isAuthenticated) {
       if (window.confirm('Are you sure you want to log out?')) {
         Cookies.remove('jwtToken');
         Cookies.remove('idToken');
-        
+        Cookies.remove('csrfTokenCookie');
+        setCsrfToken(null);
+        setJwtToken(null);
+        setIdToken(null);
         if (typeof signOut === 'function') {
           signOut();
         }
       }
     } else {
-      alert("Please SignIn or Register");
+      alert('Please SignIn or Register');
     }
   };
 
   const handleSignInClick = () => {
-    if(state.isAuthenticated){
-      alert("You are already logged in !!!")
-    } else{
+    if (state.isAuthenticated) {
+      alert('You are already logged in !!!');
+    } else {
       signIn();
-    }  
+    }
   };
 
-  useEffect(() => {
-  }, [state.isAuthenticated, showDash, showNav , showForm , showRes ,showProfile]);
-  
+
   return (
     <>
       <div>
-        <NavBar toggleNav={toggleNav} toggleDash={toggleDash} togglePro={togglePro}  />
+        <NavBar toggleNav={toggleNav} toggleDash={toggleDash} togglePro={togglePro} />
       </div>
       <div>
         {state.isAuthenticated ? (
           showDash ? (
             showForm ? (
-              <Form jwtToken={jwtToken} idToken={idToken} userDetails= {userDetails} />
-            ):
-            showRes ?(
-              <ReservationList jwtToken={jwtToken} idToken={idToken} userDetails={userDetails}  />
-            ) :
-            showFutureRes ? (
-              <FutureRes jwtToken={jwtToken} idToken={idToken} userDetails={userDetails} />
-            ):
-            (
-              <Dashboard toggleDash={toggleDash} toggleForm={toggleForm} userDetails={userDetails} toggleRes = {toggleRes} toggleFutureRes={toggleFutureRes}  />
+              <Form jwtToken={jwtToken} idToken={idToken} userDetails={userDetails} csrfToken = {csrfToken} />
+            ) : showRes ? (
+              <ReservationList jwtToken={jwtToken} idToken={idToken} userDetails={userDetails} csrfToken = {csrfToken} />
+            ) : showFutureRes ? (
+              <FutureRes jwtToken={jwtToken} idToken={idToken} userDetails={userDetails} csrfToken={csrfToken}/>
+            ) : (
+              <Dashboard
+                toggleDash={toggleDash}
+                toggleForm={toggleForm}
+                userDetails={userDetails}
+                toggleRes={toggleRes}
+                toggleFutureRes={toggleFutureRes}
+              />
             )
-          ):
-          showProfile ? (           
-              <Profile userDetails={userDetails}/>
-          )
-          : (
-            <Nav handleSignOutClick ={handleSignOutClick} handleSignInClick={handleSignInClick} />
+          ) : showProfile ? (
+            <Profile userDetails={userDetails} />
+          ) : (
+            <Nav handleSignOutClick={handleSignOutClick} handleSignInClick={handleSignInClick} />
           )
         ) : (
-          <Nav handleSignOutClick={handleSignOutClick} handleSignInClick = {handleSignInClick} />
+          <Nav handleSignOutClick={handleSignOutClick} handleSignInClick={handleSignInClick} />
         )}
       </div>
       <div>
         <Footer />
       </div>
     </>
-  );  
-}
+  );
+};
 
 export default App;
